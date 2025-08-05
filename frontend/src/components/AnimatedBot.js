fc5b7551712a107eb8c076b1ae69afcabee911a0
Agnibhakundu350 <agnijee24@gmail.com>
2025-08-05 12:16:27 +0530

    Update AnimatedBot.js

--- frontend/src/components/AnimatedBot.js
+++ frontend/src/components/AnimatedBot.js
@@ -1,1 +1,16 @@
 
+import React from 'react';
+
+const AnimatedBot = () => {
+  return (
+    <div style={{ width: '200px', height: '200px', margin: 'auto' }}>
+      <img
+        src="https://media.giphy.com/media/xT9DPB9fco7prz4Ure/giphy.gif"
+        alt="Bot"
+        style={{ width: '100%', height: '100%' }}
+      />
+    </div>
+  );
+};
+
+export default AnimatedBot;
